c86990ae589371a554cf4582eb3540664b9cd464
= <=>
2019-10-07 11:18:14 +0700

    exercise 22 java core

---
+++ app/src/at-nhatnguyen/java/asiantech/internship/summer/exercise java core/Bai22.java
@@ -0,0 +1,73 @@
+package com.example.myapplication;
+import java.util.Scanner;
+public class Bai22 {
+    public static int nhap() {
+        Scanner scan = new Scanner(System.in);
+        boolean check = false;
+        int n = 0;
+        while (!check) {
+            System.out.print(" ");
+            try {
+                n = scan.nextInt();
+                check = true;
+
+            } catch (Exception e) {
+                System.out.println("Ban phai nhap so! hay nhap lai...");
+                scan.nextLine();
+
+            }
+
+        }
+        return (n);
+    }
+
+    public static boolean checkSNT(int n) {
+        if (n > 1) {
+            for (int i = 2; i <= Math.sqrt(n); i++) {
+                if (n % i == 0) return false;
+            }
+
+            return true;
+
+        } else return false;
+
+    }
+
+    public static void lietKeUoc(int n) {
+        int count = 0;
+        System.out.print("\nCac uoc cua " + n + " la:");
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                System.out.print(" " + i);
+                count++;
+
+            }
+
+        }
+        System.out.println("\nCo " + count + " uoc");
+    }
+
+    public static void lietKeUocSNT(int n) {
+        int count = 0;
+        System.out.print("\nCac uoc cua " + n + " la:");
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0 && (checkSNT(i))) {
+                System.out.print(" " + i);
+                count++;
+
+            }
+
+        }
+        System.out.println("\nCo " + count + " uoc la so nguyen to");
+
+    }
+
+    public static void main(String[] args) {
+        System.out.print("Nhap n");
+        int n = nhap();
+        lietKeUoc(n);
+        lietKeUocSNT(n);
+
+    }
+
+}
