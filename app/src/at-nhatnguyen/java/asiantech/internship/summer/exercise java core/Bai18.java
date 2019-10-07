c1508dd32ccbb564513d170646cf746d5a88183c
= <=>
2019-10-07 11:17:52 +0700

    exercise 18 java core

---
+++ app/src/at-nhatnguyen/java/asiantech/internship/summer/exercise java core/Bai18.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import java.util.Scanner;
+import java.util.StringTokenizer;
+public class Bai18 {
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Nhap vao 1 xau: ");
+        String str = scan.nextLine();
+        StringTokenizer strToken = new StringTokenizer(str, " ");
+        System.out.println("So tu trong xau la: "+strToken.countTokens());
+    }
+}
