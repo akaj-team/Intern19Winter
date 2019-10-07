e2f594df8d737bc87b70ddbb869e6cc1e167cfb2
at-trinhnguyen2 <trinh.nguyen2@asiantech.vn>
2019-10-07 14:23:42 +0700

    exercise 32

---
+++ app/src/at-trinhnguyen/java/asiantech/internship/winter/javacore/Bai32/Shape.java
@@ -0,0 +1,13 @@
+package com.congtrinh.Bai32;
+
+public abstract class Shape {
+    abstract double area();
+
+    abstract double circumference();
+
+    void print() {
+        System.out.print("Default");
+    }
+
+
+}
