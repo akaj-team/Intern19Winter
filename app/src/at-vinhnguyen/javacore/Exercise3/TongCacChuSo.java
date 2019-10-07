58f3006f3a2d8c4619386ff8d5697f009baf6171
Vinh <vinh.nguyen@asiantech.vn>
2019-10-07 14:24:28 +0700

    Exercise3: Sum of Digits

---
+++ app/src/at-vinhnguyen/javacore/Exercise3/TongCacChuSo.java
@@ -0,0 +1,22 @@
+package Exercise3;
+
+import java.util.Scanner;
+
+public class TongCacChuSo {
+
+    public static void main(String[] args) {
+        System.out.print("Nhap so: ");
+        Scanner sn = new Scanner(System.in);
+        int a = sn.nextInt();
+        System.out.println("Tong cac chu so: "+totalAllNumbers(a));
+    }
+
+    private static int totalAllNumbers(int a) {
+        int totalAllNumbers = 0;
+        while (a > 0) {
+            totalAllNumbers += a % 10;
+            a /= 10;
+        }
+        return totalAllNumbers;
+    }
+}
