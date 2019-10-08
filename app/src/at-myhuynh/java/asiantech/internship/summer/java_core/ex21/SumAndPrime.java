48df29f88eb920a652c13c0b80ee80a186f761a7
at-myhuynh <my.huynh@asiantech.vn>
2019-10-08 21:58:29 +0700

    Sum and find prime factors

---
+++ app/src/at-myhuynh/java/asiantech/internship/summer/java_core/ex21/SumAndPrime.java
@@ -0,0 +1,23 @@
+package asiantech.internship.summer.java_core.ex21;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import asiantech.internship.summer.java_core.common.Common;
+
+public class SumAndPrime {
+
+	public static void main(String[] args) {
+		int n = 1234;
+		System.out.println("Total: " + Common.totalOfNumber(n));
+
+		List<String> strPrime = new ArrayList<>();
+		if (Common.isPrime(n)) {
+			strPrime.add(String.valueOf(n));
+		} else {
+			strPrime = Common.primeFactor(n);
+		}
+
+		System.out.println(n + " => " + String.join("x", strPrime));
+	}
+}
