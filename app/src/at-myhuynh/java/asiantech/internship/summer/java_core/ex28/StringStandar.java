7286330f6fde248bb2789e5fa4cc0115feafa70d
at-myhuynh <my.huynh@asiantech.vn>
2019-10-08 22:04:52 +0700

    Show standard string

---
+++ app/src/at-myhuynh/java/asiantech/internship/summer/java_core/ex28/StringStandar.java
@@ -0,0 +1,20 @@
+package asiantech.internship.summer.java_core.ex28;
+
+public class StringStandar {
+
+	public static void main(String[] args) {
+		String str = "    nguYen  vAn a  ";
+		System.out.println(str);
+		str = str.trim().toLowerCase();
+		String[] arrStr = str.split("[ ]+");
+
+		for (int i = 0; i < arrStr.length; i++) {
+			String temp = String.valueOf(arrStr[i].charAt(0));
+			arrStr[i] = arrStr[i].replaceFirst(temp, temp.toUpperCase());
+		}
+
+		System.out.println(String.join(" ", arrStr));
+
+	}
+
+}
