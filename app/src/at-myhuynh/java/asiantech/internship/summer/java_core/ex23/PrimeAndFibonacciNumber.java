5dfa782b251e4b8596b4a6ed61ffc88ce9afd4f7
at-myhuynh <my.huynh@asiantech.vn>
2019-10-09 16:59:05 +0700

    Change input method and CI comment

--- app/src/at-myhuynh/java/asiantech/internship/summer/java_core/ex23/PrimeAndFibonacciNumber.java
+++ app/src/at-myhuynh/java/asiantech/internship/summer/java_core/ex23/PrimeAndFibonacciNumber.java
@@ -2,16 +2,18 @@ package asiantech.internship.summer.java_core.ex23;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
 import asiantech.internship.summer.java_core.common.Common;
 
-public class PrimeAndFibonaciNumber {
+public class PrimeAndFibonacciNumber {
 
 	public static void main(String[] args) {
-		int n = 10;
+		Logger log = Logger.getLogger(PrimeAndFibonacciNumber.class.getName());
+		int n = Common.input("Nhập vào n: ");
 
-		System.out.println(n + " số nguyên tố đầu tiên: " + getListPrime(n));
-		System.out.println(n + " số Fibo đầu tiên: " + Common.getListFibonaci(n));
+		log.info(n + " số nguyên tố đầu tiên: " + getListPrime(n));
+		log.info(n + " số Fibo đầu tiên: " + Common.getFibonacci(n));
 	}
 
 	public static List<Integer> getListPrime(int n) {
