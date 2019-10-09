cb54a50ad36e2bd632635dee010f464a7b99bb54
= <=>
2019-10-09 10:17:43 +0700

    exercise 6 java core

---
+++ app/src/at-nhatnguyen/java/asiantech/internship/summer/javacore/Bai6.java
@@ -0,0 +1,34 @@
+package asiantech.internship.summer.javacore;
+
+import java.util.Scanner;
+
+public class Bai6 {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Nhap n = ");
+        int n = scan.nextInt();
+        int dem = 0;
+        int i =2;
+        System.out.printf("%d so nguyen to da tien la: \n", n);
+        while (dem < n){
+            if (PrimeNumber(i)){
+                System.out.print(i + " ");
+                dem++;
+            }
+            i++;
+        }
+    }
+
+    //điều kiện là 1 số nguyên tố
+    private static boolean PrimeNumber(int n) {
+        if (n < 2) {
+            return false;
+        }
+        for (int i = 2; i < n - 1; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
