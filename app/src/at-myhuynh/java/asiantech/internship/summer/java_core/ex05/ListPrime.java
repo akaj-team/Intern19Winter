6df428706e076de90bc72bbdda42eccd408c0b9b
at-myhuynh <my.huynh@asiantech.vn>
2019-10-09 17:14:01 +0700

    Fix CI comment

--- app/src/at-myhuynh/java/asiantech/internship/summer/java_core/ex05/ListPrime.java
+++ app/src/at-myhuynh/java/asiantech/internship/summer/java_core/ex05/ListPrime.java
@@ -1,19 +1,22 @@
 package asiantech.internship.summer.java_core.ex05;
 
+import java.util.logging.Logger;
+
 import asiantech.internship.summer.java_core.common.Common;
 
 public class ListPrime {
 
-	public static void main(String[] args) {
-		int n = Common.input("Nhập vào số n: ");
-		print(n);
-	}
+    public static void main(String[] args) {
+        int n = Common.input("Nhập vào số n: ");
+        print(n);
+    }
 
-	public static void print(int number) {
-		for (int i = 1; i <= number; i++) {
-			if (Common.isPrime(i)) {
-				System.out.println(i);
-			}
-		}
-	}
+    public static void print(int number) {
+        Logger log = Logger.getLogger(ListPrime.class.getName());
+        for (int i = 1; i <= number; i++) {
+            if (Common.isPrime(i)) {
+                log.info(String.valueOf(i));
+            }
+        }
+    }
 }
