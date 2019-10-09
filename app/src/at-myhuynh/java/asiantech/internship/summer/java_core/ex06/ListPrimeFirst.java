e489f645a810bfdb6b016523bf8fa6f015eb5b36
at-myhuynh <my.huynh@asiantech.vn>
2019-10-09 17:16:20 +0700

    Change input method - Fix CI comment

--- app/src/at-myhuynh/java/asiantech/internship/summer/java_core/ex06/ListPrimeFirst.java
+++ app/src/at-myhuynh/java/asiantech/internship/summer/java_core/ex06/ListPrimeFirst.java
@@ -1,26 +1,29 @@
 package asiantech.internship.summer.java_core.ex06;
 
 
+import java.util.logging.Logger;
+
 import asiantech.internship.summer.java_core.common.Common;
 
 public class ListPrimeFirst {
 
-	public static void main(String[] args) {
-		int n = 10;
-		print(n);
-	}
+    public static void main(String[] args) {
+        int n = Common.input("Nhập vào n: ");
+        print(n);
+    }
 
-	public static void print(int n) {
-		int count = 0;
-		int number = 1;
+    public static void print(int n) {
+        Logger log = Logger.getLogger(ListPrimeFirst.class.getName());
+        int count = 0;
+        int number = 1;
 
-		do {
-			if (Common.isPrime(number)) {
-				count++;
-				System.out.println(number);
-			}
-			number++;
-		} while (count < n);
-	}
+        do {
+            if (Common.isPrime(number)) {
+                count++;
+                log.info(String.valueOf(number));
+            }
+            number++;
+        } while (count < n);
+    }
 
 }
