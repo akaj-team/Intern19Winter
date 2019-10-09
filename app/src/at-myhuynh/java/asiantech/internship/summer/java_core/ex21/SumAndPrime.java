409a4b50eda6cbe0c9998e7b81d9f605e99b4140
at-myhuynh <my.huynh@asiantech.vn>
2019-10-09 10:57:53 +0700

    Fix error join java 8

--- app/src/at-myhuynh/java/asiantech/internship/summer/java_core/ex21/SumAndPrime.java
+++ app/src/at-myhuynh/java/asiantech/internship/summer/java_core/ex21/SumAndPrime.java
@@ -7,17 +7,17 @@ import asiantech.internship.summer.java_core.common.Common;
 
 public class SumAndPrime {
 
-	public static void main(String[] args) {
-		int n = 1234;
-		System.out.println("Total: " + Common.totalOfNumber(n));
+    public static void main(String[] args) {
+        int n = 28;
+        System.out.println("Total: " + Common.totalOfNumber(n));
 
-		List<String> strPrime = new ArrayList<>();
-		if (Common.isPrime(n)) {
-			strPrime.add(String.valueOf(n));
-		} else {
-			strPrime = Common.primeFactor(n);
-		}
+        List<String> strPrime = new ArrayList<>();
+        if (Common.isPrime(n)) {
+            strPrime.add(String.valueOf(n));
+        } else {
+            strPrime = Common.primeFactor(n);
+        }
 
-		System.out.println(n + " => " + String.join("x", strPrime));
-	}
+        System.out.println(n + " => " + Common.joinString("x", strPrime.toArray()));
+    }
 }
