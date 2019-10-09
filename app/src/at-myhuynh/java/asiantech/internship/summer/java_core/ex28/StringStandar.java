3355390af7623cf8f7ea0d0b6d33443b44ea6c38
at-myhuynh <my.huynh@asiantech.vn>
2019-10-09 11:07:57 +0700

    Delete bank line

--- app/src/at-myhuynh/java/asiantech/internship/summer/java_core/ex28/StringStandar.java
+++ app/src/at-myhuynh/java/asiantech/internship/summer/java_core/ex28/StringStandar.java
@@ -4,18 +4,17 @@ import asiantech.internship.summer.java_core.common.Common;
 
 public class StringStandar {
 
-	public static void main(String[] args) {
-		String str = "    nguYen  vAn a  ";
-		System.out.println(str);
-		str = str.trim().toLowerCase();
-		String[] arrStr = str.split("[ ]+");
+    public static void main(String[] args) {
+        String str = "    nguYen  vAn a  ";
+        System.out.println(str);
+        str = str.trim().toLowerCase();
+        String[] arrStr = str.split("[ ]+");
 
-		for (int i = 0; i < arrStr.length; i++) {
-			String temp = String.valueOf(arrStr[i].charAt(0));
-			arrStr[i] = arrStr[i].replaceFirst(temp, temp.toUpperCase());
-		}
+        for (int i = 0; i < arrStr.length; i++) {
+            String temp = String.valueOf(arrStr[i].charAt(0));
+            arrStr[i] = arrStr[i].replaceFirst(temp, temp.toUpperCase());
+        }
 
-		System.out.println(Common.joinString(" ", arrStr));
-
-	}
+        System.out.println(Common.joinString(" ", arrStr));
+    }
 }
