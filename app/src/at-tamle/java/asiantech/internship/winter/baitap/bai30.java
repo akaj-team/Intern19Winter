b90c79eb0c0622d865e45d01c575843dc7dc28f7
at-tamle <at-tamle@asiantech.vn>
2019-10-09 11:34:12 +0700

    ex30

--- app/src/at-tamle/java/asiantech/internship/winter/baitap/bai30.java
+++ app/src/at-tamle/java/asiantech/internship/winter/baitap/bai30.java
@@ -1,24 +1,29 @@
 package asiantech.internship.winter.baitap;
+
 import java.util.*;
+
 // ho dem ten > ten ho dem
 public class bai30 {
-	 public static void chuyendoi(String s){
-	        StringTokenizer tg= new StringTokenizer(s);
-	        StringBuffer kq= new StringBuffer();
-	        String s1[]= new String[10];
-	        int i=1;
-	        while(tg.hasMoreTokens()){
-	            s1[i]= tg.nextToken();
-	            i++;
-	        }
-	        kq.append(s1[3]).append("  "); kq.append(s1[1]).append("  "); kq.append(s1[2]).append("  ");
-	       System.out.println(kq);
-	    }
-	    public static void main(String[] args) {
-	        String s0= new String();
-	        Scanner in= new Scanner(System.in);
-	        System.out.println("nhap xau ho ten: ");
-	        s0= in.nextLine();
-	        chuyendoi(s0);
-	  }
+    public static void chuyendoi(String s) {
+        StringTokenizer tg = new StringTokenizer(s);
+        StringBuffer kq = new StringBuffer();
+        String s1[] = new String[10];
+        int i = 1;
+        while (tg.hasMoreTokens()) {
+            s1[i] = tg.nextToken();
+            i++;
+        }
+        kq.append(s1[3]).append("  ");
+        kq.append(s1[1]).append("  ");
+        kq.append(s1[2]).append("  ");
+        System.out.println(kq);
+    }
+
+    public static void main(String[] args) {
+        String s0 = new String();
+        Scanner in = new Scanner(System.in);
+        System.out.println("nhap xau ho ten: ");
+        s0 = in.nextLine();
+        chuyendoi(s0);
+    }
 }
