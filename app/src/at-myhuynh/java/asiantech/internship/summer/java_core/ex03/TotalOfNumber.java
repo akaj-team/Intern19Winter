d650d424e02a17b5476f69da38b4a6d4acab8062
at-myhuynh <my.huynh@asiantech.vn>
2019-10-09 17:10:15 +0700

    Fix CI comment

--- app/src/at-myhuynh/java/asiantech/internship/summer/java_core/ex03/TotalOfNumber.java
+++ app/src/at-myhuynh/java/asiantech/internship/summer/java_core/ex03/TotalOfNumber.java
@@ -1,13 +1,16 @@
 package asiantech.internship.summer.java_core.ex03;
 
+import java.util.logging.Logger;
+
 import asiantech.internship.summer.java_core.common.Common;
 
 public class TotalOfNumber {
 
     public static void main(String[] args) {
+        Logger log = Logger.getLogger(TotalOfNumber.class.getName());
         int number = Common.input("Nhập vào số cần tính: ");
         int total = Common.totalOfNumber(number);
 
-        System.out.println(number + " => " + total);
+        log.info(number + " => " + total);
     }
 }
