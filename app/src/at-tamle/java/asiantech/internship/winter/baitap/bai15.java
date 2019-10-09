8ac6c823a48893f75474a497cea5ec86abf7ff3e
at-tamle <at-tamle@asiantech.vn>
2019-10-09 09:15:29 +0700

    fileluu

--- app/src/at-tamle/java/asiantech/internship/winter/baitap/bai15.java
+++ app/src/at-tamle/java/asiantech/internship/winter/baitap/bai15.java
@@ -1,6 +1,6 @@
 package asiantech.internship.winter.baitap;
 import java.util.*;
-// liet ke cac ptu trong day xuat hien 2 lan
+//  liet ke cac ptu trong day xuat hien 2 lan
 public class bai15 {
 	
 	  public static int nhap(){
