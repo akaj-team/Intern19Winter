3f76bb19cb011df4d6226a4bc7b75cf9a04de215
at-myhuynh <my.huynh@asiantech.vn>
2019-10-09 13:55:02 +0700

    Change function name - format

--- app/src/at-myhuynh/java/asiantech/internship/summer/java_core/common/Common.java
+++ app/src/at-myhuynh/java/asiantech/internship/summer/java_core/common/Common.java
@@ -3,101 +3,101 @@ package asiantech.internship.summer.java_core.common;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.logging.Logger;
 
 public class Common {
+    private static Logger log = Logger.getLogger(Common.class.getName());
 
-	public static int input(String text) {
-		@SuppressWarnings("resource")
-		Scanner sc = new Scanner(System.in);
-		int n = 0;
-		boolean check = true;
-		
-		do {
-			try {
-				System.out.print(text);
-				n = Integer.parseInt(sc.nextLine());
-				check = false;
-			} catch (Exception e) {
-				check = true;
-				System.out.println(e);
-				System.err.println("Nhập lại!!");
-			}
-		} while (check);
-		
-		return n;
-	}
+    public static int input(String text) {
+        Scanner sc = new Scanner(System.in, "UTF-8");
+        int n = 0;
+        boolean check;
 
-	public static boolean thuanNghich(String text) {
-		return text.equals(new StringBuffer(text).reverse().toString());
-	}
+        do {
+            try {
+                log.info(text);
+                n = Integer.parseInt(sc.nextLine());
+                check = false;
+            } catch (Exception e) {
+                check = true;
+                e.printStackTrace();
+                log.info("Nhập lại!!");
+            }
+        } while (check);
 
-	public static boolean isPrime(int number) {
-		if (number == 1 || number == 2) {
-			return true;
-		} else {
-			for (int i = 2; i <= number / 2; i++) {
-				if (number % i == 0) {
-					return false;
-				}
-			}
-		}
-		return true;
-	}
+        return n;
+    }
 
-	public static List<String> primeFactor(int number) {
-		List<String> listPrime = new ArrayList<>();
-		int index = 2;
-		while (number >= index) {
-			if (number % index == 0) {
-				listPrime.add(String.valueOf(index));
-				number /= index;
-			} else {
-				index++;
-			}
-		}
-		return listPrime;
-	}
+    public static boolean isChainOfSymmetry(String text) {
+        return text.equals(new StringBuffer(text).reverse().toString());
+    }
 
-	public static int totalOfNumber(int number) {
-		int total = 0;
+    public static boolean isPrime(int number) {
+        if (number == 1 || number == 2) {
+            return true;
+        } else {
+            for (int i = 2; i <= number / 2; i++) {
+                if (number % i == 0) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 
-		while (number > 0) {
-			total += number % 10;
-			number /= 10;
-		}
-		return total;
-	}
+    public static List<String> primeFactor(int number) {
+        List<String> listPrime = new ArrayList<>();
+        int index = 2;
+        while (number >= index) {
+            if (number % index == 0) {
+                listPrime.add(String.valueOf(index));
+                number /= index;
+            } else {
+                index++;
+            }
+        }
+        return listPrime;
+    }
 
-	public static List<Integer> getListFibonaci(int n) {
-		List<Integer> listFibo = new ArrayList<>();
-		int count = 2;
-		int f1 = 1;
-		int f2 = 1;
-		int fn = 1;
+    public static int totalOfNumber(int number) {
+        int total = 0;
 
-		listFibo.add(f1);
-		listFibo.add(f2);
+        while (number > 0) {
+            total += number % 10;
+            number /= 10;
+        }
+        return total;
+    }
 
-		do {
-			fn = f1 + f2;
-			f1 = f2;
-			f2 = fn;
-			listFibo.add(fn);
-			count++;
-		} while (count < n);
+    public static List<Integer> getFibonacci(int n) {
+        List<Integer> listFibo = new ArrayList<>();
+        int count = 2;
+        int f1 = 1;
+        int f2 = 1;
 
-		return listFibo;
-	}
+        listFibo.add(f1);
+        listFibo.add(f2);
 
-	public static String joinString(String delimiter, Object[] arrStr) {
-		String result = "";
-		for (int i = 0; i < arrStr.length; i++) {
-			if (i == arrStr.length - 1) {
-				result += arrStr[i];
-			} else {
-				result += arrStr[i] + delimiter;
-			}
-		}
-		return result;
-	}
+        do {
+            int fn = f1 + f2;
+            f1 = f2;
+            f2 = fn;
+            listFibo.add(fn);
+            count++;
+        } while (count < n);
+
+        return listFibo;
+    }
+
+    public static String joinString(String delimiter, Object[] arrStr) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < arrStr.length; i++) {
+            if (i == arrStr.length - 1) {
+                result.append(arrStr[i]);
+            } else {
+                result.append(arrStr[i]).append(delimiter);
+            }
+        }
+        return result.toString();
+    }
 }
