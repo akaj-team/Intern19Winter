218bf790a3f5ecd64383f9db23f65c2e0da8b606
at-myhuynh <my.huynh@asiantech.vn>
2019-10-10 10:30:08 +0700

    Add input number

---
+++ app/src/at-myhuynh/java/asiantech/internship/summer/java_core/ex20/FibonacciPrime.java
@@ -0,0 +1,28 @@
+package asiantech.internship.summer.java_core.ex20;
+
+import java.util.logging.Logger;
+
+import asiantech.internship.summer.java_core.common.Common;
+
+public class FibonacciPrime {
+    public static void main(String[] args) {
+        int n = Common.input("Nhập vào số n:");
+        showFibonacciIsPrime(n);
+    }
+
+    public static void showFibonacciIsPrime(int n) {
+        Logger log = Logger.getLogger(FibonacciPrime.class.getName());
+        int f1 = 1;
+        int f2 = 1;
+        int fn = 1;
+
+        do {
+            if (fn < n && Common.isPrime(fn)) {
+                log.info(String.valueOf(fn));
+            }
+            fn = f1 + f2;
+            f1 = f2;
+            f2 = fn;
+        } while (fn < n);
+    }
+}
