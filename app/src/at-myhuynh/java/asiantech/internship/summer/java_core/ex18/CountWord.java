dab4310949b1bff9475568f5f5ba7cd08aba3bda
at-myhuynh <my.huynh@asiantech.vn>
2019-10-10 10:23:05 +0700

    Fix split string

--- app/src/at-myhuynh/java/asiantech/internship/summer/java_core/ex18/CountWord.java
+++ app/src/at-myhuynh/java/asiantech/internship/summer/java_core/ex18/CountWord.java
@@ -1,11 +1,16 @@
 package asiantech.internship.summer.java_core.ex18;
 
+import java.util.logging.Logger;
+
+import asiantech.internship.summer.java_core.common.Common;
+
 public class CountWord {
 
-	public static void main(String[] args) {
-		String strWord = "Truong hoc cua em";
-		int count = strWord.split(" ").length;
-		System.out.println("'" + strWord + "' có " + count + " từ.");
-	}
+    public static void main(String[] args) {
+        Logger log = Logger.getLogger(CountWord.class.getName());
+        String strWord = Common.inputString("Nhập vào chuỗi: ");
+        int count = strWord.split("[ ]+").length;
+        log.info("'" + strWord + "' có " + count + " từ.");
+    }
 
 }
