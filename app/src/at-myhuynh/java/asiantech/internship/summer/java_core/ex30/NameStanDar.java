dc5fd2145d2010db23e99d8281d868f7702af1b8
at-myhuynh <my.huynh@asiantech.vn>
2019-10-10 11:18:55 +0700

    Add check tab - multiple space

--- app/src/at-myhuynh/java/asiantech/internship/summer/java_core/ex30/NameStanDar.java
+++ app/src/at-myhuynh/java/asiantech/internship/summer/java_core/ex30/NameStanDar.java
@@ -1,16 +1,22 @@
 package asiantech.internship.summer.java_core.ex30;
 
+import java.util.logging.Logger;
+
+import asiantech.internship.summer.java_core.common.Common;
+
 public class NameStanDar {
 
-	public static void main(String[] args) {
-		String name = "Nguyễn Văn A";
-		
-		String firstName = name.substring(0, name.indexOf(" "));
-		String middleName = name.substring(name.indexOf(" ") + 1, name.lastIndexOf(" "));
-		String lastName = name.substring(name.lastIndexOf(" ") + 1, name.length());
+    public static void main(String[] args) {
+        Logger log = Logger.getLogger(NameStanDar.class.getName());
+        String name = Common.inputString("Nhập vào họ tên:");
+        name = name .replaceAll("\\s+", " ").trim();
 
-		String nameStand = lastName + " " + middleName + " " + firstName;
-		System.out.println(name + " => " + nameStand);
-	}
+        String firstName = name.substring(0, name.indexOf(" "));
+        String middleName = name.substring(name.indexOf(" ") + 1, name.lastIndexOf(" "));
+        String lastName = name.substring(name.lastIndexOf(" ") + 1);
+
+        String nameStand = lastName + " " + middleName + " " + firstName;
+        log.info(name + " => " + nameStand);
+    }
 
 }
