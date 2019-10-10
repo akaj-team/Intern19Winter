28f63645dc56f66915f897d0cf2fdda297361add
at-myhuynh <my.huynh@asiantech.vn>
2019-10-10 10:57:58 +0700

    Fix CI comment with string method

--- app/src/at-myhuynh/java/asiantech/internship/summer/java_core/ex28/StringStanDar.java
+++ app/src/at-myhuynh/java/asiantech/internship/summer/java_core/ex28/StringStanDar.java
@@ -1,5 +1,6 @@
 package asiantech.internship.summer.java_core.ex28;
 
+import java.util.Locale;
 import java.util.logging.Logger;
 
 import asiantech.internship.summer.java_core.common.Common;
@@ -10,12 +11,12 @@ public class StringStanDar {
         Logger log = Logger.getLogger(StringStanDar.class.getName());
         String str = Common.inputString("Nhập vào chuỗi: ");
         log.info(str);
-        str = str.trim().toLowerCase();
+        str = str.trim().toLowerCase(Locale.getDefault());
         String[] arrStr = str.split("[ ]+");
 
         for (int i = 0; i < arrStr.length; i++) {
             String temp = String.valueOf(arrStr[i].charAt(0));
-            arrStr[i] = arrStr[i].replaceFirst(temp, temp.toUpperCase());
+            arrStr[i] = arrStr[i].replaceFirst(temp, temp.toUpperCase(Locale.getDefault()));
         }
 
         log.info(Common.joinString(" ", arrStr));
