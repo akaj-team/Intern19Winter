37fb1ca6f4cc5811d4a985ea84553910c44297f6
at-myhuynh <my.huynh@asiantech.vn>
2019-10-10 10:32:06 +0700

    Fix input - CI comment

--- app/src/at-myhuynh/java/asiantech/internship/summer/java_core/ex21/SumAndPrime.java
+++ app/src/at-myhuynh/java/asiantech/internship/summer/java_core/ex21/SumAndPrime.java
@@ -2,14 +2,16 @@ package asiantech.internship.summer.java_core.ex21;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
 import asiantech.internship.summer.java_core.common.Common;
 
 public class SumAndPrime {
 
     public static void main(String[] args) {
-        int n = 28;
-        System.out.println("Total: " + Common.totalOfNumber(n));
+        Logger log = Logger.getLogger(SumAndPrime.class.getName());
+        int n = Common.input("Nhập vào 1 số:");
+        log.info("Total: " + Common.totalOfNumber(n));
 
         List<String> strPrime = new ArrayList<>();
         if (Common.isPrime(n)) {
@@ -18,6 +20,6 @@ public class SumAndPrime {
             strPrime = Common.primeFactor(n);
         }
 
-        System.out.println(n + " => " + Common.joinString("x", strPrime.toArray()));
+        log.info(n + " => " + Common.joinString("x", strPrime.toArray()));
     }
 }
