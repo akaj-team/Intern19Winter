0b7600e16b39626358b30ea388f4955e068dade5
at-myhuynh <my.huynh@asiantech.vn>
2019-10-10 10:25:29 +0700

    Add input number

--- app/src/at-myhuynh/java/asiantech/internship/summer/java_core/ex19/SumNumberPrime.java
+++ app/src/at-myhuynh/java/asiantech/internship/summer/java_core/ex19/SumNumberPrime.java
@@ -1,15 +1,18 @@
 package asiantech.internship.summer.java_core.ex19;
 
+import java.util.logging.Logger;
+
 import asiantech.internship.summer.java_core.common.Common;
 
 public class SumNumberPrime {
-	public static void main(String[] args) {
-		int n = 14;
-		for(int i = 10000; i < 100000; i++){
-			if(Common.isPrime(i) && Common.totalOfNumber(i) == n){
-				System.out.println(i);
-			}
-		}
+    public static void main(String[] args) {
+        Logger log = Logger.getLogger(SumNumberPrime.class.getName());
+        int n = Common.input("Nhập vào số n:");
+        for (int i = 10000; i < 100000; i++) {
+            if (Common.isPrime(i) && Common.totalOfNumber(i) == n) {
+                log.info(String.valueOf(i));
+            }
+        }
 
-	}
+    }
 }
