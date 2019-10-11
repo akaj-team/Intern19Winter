d99f87a056507de6c4ae617b48b191a279caaabb
at-myhuynh <my.huynh@asiantech.vn>
2019-10-11 09:10:13 +0700

    Change string to char

--- app/src/at-myhuynh/java/asiantech/internship/summer/java_core/ex30/NameStanDar.java
+++ app/src/at-myhuynh/java/asiantech/internship/summer/java_core/ex30/NameStanDar.java
@@ -1,5 +1,6 @@
 package asiantech.internship.summer.java_core.ex30;
 
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import asiantech.internship.summer.java_core.common.Common;
@@ -9,14 +10,16 @@ public class NameStanDar {
     public static void main(String[] args) {
         Logger log = Logger.getLogger(NameStanDar.class.getName());
         String name = Common.inputString("Nhập vào họ tên:");
-        name = name .replaceAll("\\s+", " ").trim();
+        name = name.replaceAll("\\s+", " ").trim();
 
-        String firstName = name.substring(0, name.indexOf(" "));
-        String middleName = name.substring(name.indexOf(" ") + 1, name.lastIndexOf(" "));
-        String lastName = name.substring(name.lastIndexOf(" ") + 1);
+        String firstName = name.substring(0, name.indexOf(' '));
+        String middleName = name.substring(name.indexOf(' ') + 1, name.lastIndexOf(' '));
+        String lastName = name.substring(name.lastIndexOf(' ') + 1);
 
         String nameStand = lastName + " " + middleName + " " + firstName;
-        log.info(name + " => " + nameStand);
+        if (log.isLoggable(Level.INFO)) {
+            log.info(name + " => " + nameStand);
+        }
     }
 
 }
