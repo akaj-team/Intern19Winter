83107ab1d6f4b83acd8ce9658051b0745692b8a8
Vinh <vinh.nguyen@asiantech.vn>
2019-10-11 16:53:40 +0700

    List Fibonacci Numbers

---
+++ app/src/at-vinhnguyen/java/asiantech/internship/summer/javacore/excercise20/ListFibonacciNumbers.java
@@ -0,0 +1,46 @@
+package asiantech.internship.summer.javacore.excercise20;
+
+import java.util.Scanner;
+
+public class ListFibonacciNumbers {
+
+    private static Scanner sn = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.print("Nhập số tự nhiên n = ");
+        int n = sn.nextInt();
+        System.out.print("Các số fibonacci nhỏ hơn " + n + " và "
+                + "là số nguyên tố: ");
+        int i = 0;
+        while (fibonacci(i) < n) {
+            int fi = fibonacci(i);
+            if (isPrimeNumber(fi)) {
+                System.out.print(fi + " ");
+            }
+            i++;
+        }
+    }
+
+    public static int fibonacci(int n) {
+        if (n < 0) {
+            return -1;
+        } else if (n == 0 || n == 1) {
+            return n;
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
+
+    public static boolean isPrimeNumber(int n) {
+        if (n < 2) {
+            return false;
+        }
+        int squareRoot = (int) Math.sqrt(n);
+        for (int i = 2; i <= squareRoot; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
