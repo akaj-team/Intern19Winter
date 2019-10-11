6837bec36b20ba5088213bfd88b8b7d9c61fbe0f
at-myhuynh <my.huynh@asiantech.vn>
2019-10-11 09:19:28 +0700

    Add print prime and fibo number to if block

--- app/src/at-myhuynh/java/asiantech/internship/summer/java_core/ex23/PrimeAndFibonacciNumber.java
+++ app/src/at-myhuynh/java/asiantech/internship/summer/java_core/ex23/PrimeAndFibonacciNumber.java
@@ -2,32 +2,35 @@ package asiantech.internship.summer.java_core.ex23;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import asiantech.internship.summer.java_core.common.Common;
 
 public class PrimeAndFibonacciNumber {
 
-	public static void main(String[] args) {
-		Logger log = Logger.getLogger(PrimeAndFibonacciNumber.class.getName());
-		int n = Common.input("Nhập vào n: ");
+    public static void main(String[] args) {
+        Logger log = Logger.getLogger(PrimeAndFibonacciNumber.class.getName());
+        int n = Common.input("Nhập vào n: ");
 
-		log.info(n + " số nguyên tố đầu tiên: " + getListPrime(n));
-		log.info(n + " số Fibo đầu tiên: " + Common.getFibonacci(n));
-	}
+        if (log.isLoggable(Level.INFO)) {
+            log.info(n + " số nguyên tố đầu tiên: " + getListPrime(n));
+            log.info(n + " số Fibo đầu tiên: " + Common.getFibonacci(n));
+        }
+    }
 
-	public static List<Integer> getListPrime(int n) {
-		List<Integer> listPrime = new ArrayList<>();
-		int count = 1;
-		int index = 1;
-		while (count <= n) {
-			if (Common.isPrime(index)) {
-				listPrime.add(index);
-				count++;
-			}
-			index++;
-		}
-		return listPrime;
-	}
+    public static List<Integer> getListPrime(int n) {
+        List<Integer> listPrime = new ArrayList<>();
+        int count = 1;
+        int index = 1;
+        while (count <= n) {
+            if (Common.isPrime(index)) {
+                listPrime.add(index);
+                count++;
+            }
+            index++;
+        }
+        return listPrime;
+    }
 
 }
