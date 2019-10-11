220508361634e48a6b43bff37a545a2c13894f1a
Vinh <vinh.nguyen@asiantech.vn>
2019-10-11 16:51:27 +0700

    List Permutations

---
+++ app/src/at-vinhnguyen/java/asiantech/internship/summer/javacore/excercise11/ListPermutations.java
@@ -0,0 +1,48 @@
+package asiantech.internship.summer.javacore.excercise11;
+
+import java.util.Scanner;
+
+public class ListPermutations {
+
+    public static void main(String[] args) {
+        System.out.print("Nhap n: ");
+        int n = new Scanner(System.in).nextInt();
+        doListPermutations(n);
+    }
+
+    private static void doListPermutations(int n) {
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = i + 1;
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.print("" + a[i]);
+        }
+        for (int i = n - 1; i > 0; i--) {
+
+            if (a[i] > a[i - 1]) {
+
+                for (int j = n - 1; j >= i; j--) {
+                    if (a[j] > a[i - 1]) {
+                        int temp = a[j];
+                        a[j] = a[i - 1];
+                        a[i - 1] = temp;
+                        break;
+                    }
+                }
+
+                for (int j = n - 1; j > ((n - 1 + i) / 2); j--) {
+                    int temp = a[i + n - 1 - j];
+                    a[i + n - 1 - j] = a[j];
+                    a[j] = temp;
+                }
+
+                System.out.println("");
+                for (int j = 0; j < n; j++) {
+                    System.out.print("" + a[j]);
+                }
+                i = n;
+            }
+        }
+    }
+}
