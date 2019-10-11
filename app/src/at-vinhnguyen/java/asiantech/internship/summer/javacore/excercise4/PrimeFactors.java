5725a3de6e5778dc351daee563e74d01eed7854c
Vinh <vinh.nguyen@asiantech.vn>
2019-10-11 16:41:23 +0700

    Prime Factors

---
+++ app/src/at-vinhnguyen/java/asiantech/internship/summer/javacore/excercise4/PrimeFactors.java
@@ -0,0 +1,30 @@
+package asiantech.internship.summer.javacore.excercise4;
+
+import static asiantech.internship.summer.javacore.excercise21.Bait21.isPrime;
+
+import java.util.Scanner;
+
+public class PrimeFactors {
+
+    public static void main(String[] args) {
+        System.out.println("Nhap n: ");
+        int n = new Scanner(System.in).nextInt();
+        doFactorAnalysis(n);
+    }
+
+    private static void doFactorAnalysis(int n) {
+        int i = 2;
+        while (n > 1) {
+            if (isPrime(i)) {
+                if (n % i == 0) {
+                    System.out.println(i + ".");
+                    n /= i;
+                } else {
+                    i++;
+                }
+            } else {
+                i++;
+            }
+        }
+    }
+}
