5a77a4512311e1089e6fd1c69d32971659ad2e9d
at-tamle <tam.le2@asiantech.vn>
2019-10-14 15:12:38 +0700

    bai21

--- app/src/at-tamle/java/asiantech/internship/winter/ex21/Totalanalytical.java
+++ app/src/at-tamle/java/asiantech/internship/winter/ex21/Totalanalytical.java
@@ -2,7 +2,7 @@ package asiantech.internship.winter.ex21;
 
 import java.util.Scanner;
 //tinh tong + phan tich thua so ngto
-public class totalanalytical {
+public class Totalanalytical {
     public static int process(){
         Scanner input= new Scanner(System.in); boolean check= false;
         int n=0; while(!check){
