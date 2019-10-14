f9fca9b7b51d8489ce1eae274165199fca80545f
at-myhuynh <my.huynh@asiantech.vn>
2019-10-14 23:49:58 +0700

    Count words

---
+++ app/src/at-myhuynh/java/asiantech/internship/summer/kotlin/ex18/CountWord.kt
@@ -0,0 +1,15 @@
+package asiantech.internship.summer.kotlin.ex18
+
+import asiantech.internship.summer.kotlin.common.Common
+
+object CountWord {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val strWord = Common.inputString("Nhập vào chuỗi: ")
+        val count =
+            strWord.split("\\s+".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray().size
+        println("'$strWord' có $count từ.")
+    }
+
+}
