d4f7d55052db71e701db4e01de3aa2e58e8872b3
at-myhuynh <my.huynh@asiantech.vn>
2019-10-15 00:02:49 +0700

    Show standard string

---
+++ app/src/at-myhuynh/java/asiantech/internship/summer/kotlin/ex28/StringStandardize.kt
@@ -0,0 +1,25 @@
+package asiantech.internship.summer.kotlin.ex28
+
+import asiantech.internship.summer.kotlin.common.Common
+import java.util.*
+
+object StringStandardize {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        var str = Common.inputString("Nhập vào chuỗi: ")
+        println(str)
+        str = str.trim().toLowerCase(Locale.getDefault())
+        val arrStr =
+                str.split("\\s+".toRegex()).dropWhile(String::isEmpty).dropLastWhile(String::isEmpty)
+                        .toTypedArray()
+
+        for (i in arrStr.indices) {
+            val temp = arrStr[i][0].toString()
+            arrStr[i] =
+                    arrStr[i].replaceFirst(temp.toRegex(), temp.toUpperCase(Locale.getDefault()))
+        }
+
+        println(arrStr.joinToString(" ", "", "", arrStr.size, ""))
+    }
+}
