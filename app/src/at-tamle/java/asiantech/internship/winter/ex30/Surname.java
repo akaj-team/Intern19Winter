4d62f1f759796b8f7c24404afcd09dde7fdf6ec9
at-tamle <tam.le2@asiantech.vn>
2019-10-14 15:17:04 +0700

    bai30

--- app/src/at-tamle/java/asiantech/internship/winter/ex30/Surname.java
+++ app/src/at-tamle/java/asiantech/internship/winter/ex30/Surname.java
@@ -2,7 +2,7 @@ package asiantech.internship.winter.ex30;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 // ho dem ten > ten ho dem
-public class surname {
+public class Surname {
     private static void total(String s) {
         StringTokenizer t = new StringTokenizer(s);
         StringBuffer k = new StringBuffer();
