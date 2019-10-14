2e2ae23c24cd916c94d2cd8a8c12e151f0869789
at-myhuynh <my.huynh@asiantech.vn>
2019-10-14 23:38:26 +0700

    Find first primes

---
+++ app/src/at-myhuynh/java/asiantech/internship/summer/kotlin/ex06/ListPrimeFirst.kt
@@ -0,0 +1,27 @@
+package asiantech.internship.summer.kotlin.ex06
+
+import asiantech.internship.summer.kotlin.common.Common
+import asiantech.internship.summer.kotlin.common.isPrime
+
+object ListPrimeFirst {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val n = Common.input("Nhập vào n: ")
+        print(n)
+    }
+
+    private fun print(n: Int) {
+        var i = n
+        var number = 1
+
+        do {
+            if (number.isPrime()) {
+                i--
+                println(number)
+            }
+            number++
+        } while (i > 0)
+    }
+
+}
