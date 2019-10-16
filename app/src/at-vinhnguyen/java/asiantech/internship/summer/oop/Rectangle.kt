c10a28eea686e85b28be17b5f67990dcc61582d2
Vinh <vinh.nguyen@asiantech.vn>
2019-10-16 09:58:46 +0700

    OOP Excercise

---
+++ app/src/at-vinhnguyen/java/asiantech/internship/summer/oop/Rectangle.kt
@@ -0,0 +1,21 @@
+package oop
+
+class Rectangle(private val a: Float, private val b: Float) : Shape() {
+
+    override fun Acreage(): Float {
+        return a * b
+    }
+
+    override fun Perimeter(): Float {
+        return (a + b) * 2
+    }
+
+    override fun printNameOfTheShape() {
+        if (a == b) {
+            super.printNameOfTheShape()
+            return
+        }
+        println("Rectangle:")
+    }
+
+}
---
+++ app/src/at-vinhnguyen/java/asiantech/internship/summer/oop/Shape.kt
@@ -0,0 +1,12 @@
+package oop
+
+abstract class Shape {
+
+    internal open fun printNameOfTheShape() {
+        println("Square:")
+    }
+
+    internal abstract fun Acreage(): Float
+
+    internal abstract fun Perimeter(): Float
+}
