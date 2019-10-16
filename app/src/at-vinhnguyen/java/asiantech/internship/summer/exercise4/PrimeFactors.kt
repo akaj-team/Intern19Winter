79fddfea407d801390efafd7cab9abd5dcbf64a9
Vinh <vinh.nguyen@asiantech.vn>
2019-10-16 09:47:32 +0700

    Prime Factors

---
+++ app/src/at-vinhnguyen/java/asiantech/internship/summer/exercise4/PrimeFactors.kt
@@ -0,0 +1,14 @@
+package exercise4
+
+import doFactorAnalysis
+import inputIntNumber
+
+object PrimeFactors {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        print("Nhap n:")
+        val n = inputIntNumber()
+        n.doFactorAnalysis()
+    }
+}
