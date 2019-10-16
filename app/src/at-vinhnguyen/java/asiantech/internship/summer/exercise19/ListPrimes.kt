ceadfe7f7bbfb3fa856fffee76fb67457265c367
Vinh <vinh.nguyen@asiantech.vn>
2019-10-16 09:53:06 +0700

    List Primes

---
+++ app/src/at-vinhnguyen/java/asiantech/internship/summer/exercise19/ListPrimes.kt
@@ -0,0 +1,23 @@
+package exercise19
+
+import inputIntNumber
+import isPrime
+import sumOfNumber
+
+object ListPrimes {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val n = inputIntNumber()
+        println("Liệt kê tất cả số nguyên tố có 5 chữ số có tổng các chữ số bằng: $n")
+        var i = 10001
+        while (i < 99999) {
+            if (i.isPrime()) {
+                if (n == i.sumOfNumber()) {
+                    println(i)
+                }
+            }
+            i += 2
+        }
+    }
+}
