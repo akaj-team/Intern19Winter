b51b358fb8c9b43e66b93b78ab2db937429f75a1
Vinh <vinh.nguyen@asiantech.vn>
2019-10-16 09:51:40 +0700

    The numbe of occurrences of the element

---
+++ app/src/at-vinhnguyen/java/asiantech/internship/summer/exercise16/TheNumberOfOccurrencesOfTheElement.kt
@@ -0,0 +1,35 @@
+package exercise16
+
+import inputIntNumber
+import java.util.*
+
+object TheNumberOfOccurrencesOfTheElement {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        print("Nhập số phần tử của mảng: ")
+        val n = inputIntNumber()
+        val arr = IntArray(n)
+        print("Nhập các phần tử của mảng: \n")
+        for (i in 0 until n) {
+            System.out.printf("a[%d] = ", i)
+            arr[i] = inputIntNumber()
+        }
+        val map = TreeMap<Int, Int>()
+        for (i in 0 until n) {
+            addElement(map, arr[i])
+        }
+        for (key in map.keys) {
+            System.out.printf("%d xuất hiện %d lần.\n", key, map[key])
+        }
+    }
+
+    private fun addElement(map: MutableMap<Int, Int>, element: Int) {
+        if (map.containsKey(element)) {
+            var count = map[element]!! + 1
+            map[element] = count
+        } else {
+            map[element] = 1
+        }
+    }
+}
