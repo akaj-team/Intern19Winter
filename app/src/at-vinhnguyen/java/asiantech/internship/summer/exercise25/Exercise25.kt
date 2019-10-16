9ac8a66449da550eb98213445227069415ffa7e7
Vinh <vinh.nguyen@asiantech.vn>
2019-10-16 09:56:33 +0700

    Exercise25

---
+++ app/src/at-vinhnguyen/java/asiantech/internship/summer/exercise25/Exercise25.kt
@@ -0,0 +1,23 @@
+package exercise25
+
+import isPrime
+import isReversible
+import sumOfNumber
+
+object Exercise25 {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        var count = 0
+        println("cac so co 7 chu so thoa man dieu kien la: ")
+        var i = 1000001
+        while (i < 9999999) {
+            if (i.isPrime() && i.sumOfNumber().isReversible() && i.isReversible()) {
+                println(" $i")
+                count++
+            }
+            i += 2
+        }
+        println("\n Co $count so thoa man")
+    }
+}
