632327a299c2b270b7a88d9f2054b83c54dacc6e
Vinh <vinh.nguyen@asiantech.vn>
2019-10-16 09:49:02 +0700

    Reversible

---
+++ app/src/at-vinhnguyen/java/asiantech/internship/summer/exercise8/Reversible.kt
@@ -0,0 +1,18 @@
+package exercise8
+
+import java.util.*
+
+object Reversible {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        println("Nhap chuoi: ")
+        val str = Scanner(System.`in`).nextLine()
+        val strBuilder = StringBuilder(str)
+        if (strBuilder.reverse().toString().equals(str)) {
+            println("Chuoi thuan nghich doc")
+        } else {
+            println("Khong phai chuoi thuan nghich doc")
+        }
+    }
+}
