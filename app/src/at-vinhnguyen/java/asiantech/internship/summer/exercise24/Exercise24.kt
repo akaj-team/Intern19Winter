824502feedb18b6f5a2385c9368072babb380cc3
Vinh <vinh.nguyen@asiantech.vn>
2019-10-16 09:56:13 +0700

    Exercise24

---
+++ app/src/at-vinhnguyen/java/asiantech/internship/summer/exercise24/Exercise24.kt
@@ -0,0 +1,32 @@
+package exercise24
+
+import isPrime
+import isReversible
+
+object Exercise24 {
+    private fun isEachDigitIsAPrimeNumber(number: Int): Boolean {
+        var n = number
+        while (n != 0) {
+            if (!(n % 10).isPrime()) {
+                return false
+            }
+            n /= 10
+        }
+        return true
+    }
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        var count = 0
+        println("cac so tu 5-7 chu so thoa man dieu kien la: ")
+        var i = 10001
+        while (i < 10000000) {
+            if (i.isPrime() && isEachDigitIsAPrimeNumber(i) && i.isReversible()) {
+                println(" $i")
+                count++
+            }
+            i += 2
+        }
+        println("\n Co $count so thoa man")
+    }
+}
