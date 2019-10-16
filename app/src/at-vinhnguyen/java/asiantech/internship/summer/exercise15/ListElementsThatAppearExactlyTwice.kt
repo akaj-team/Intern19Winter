206dacb82428387540420529e3cb8f943f2d39b9
Vinh <vinh.nguyen@asiantech.vn>
2019-10-16 09:51:09 +0700

    List Elements That Apprear Twice

---
+++ app/src/at-vinhnguyen/java/asiantech/internship/summer/exercise15/ListElementsThatAppearExactlyTwice.kt
@@ -0,0 +1,39 @@
+package exercise15
+
+import inputIntNumber
+
+object ListElementsThatAppearExactlyTwice {
+
+    fun countElement(a: IntArray, n: Int, i: Int): Int {
+        var count = 0
+        for (j in 0 until n) {
+            if (a[j] == i) {
+                count++
+            }
+        }
+        return count
+    }
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val n: Int
+        var i: Int
+        print("Nhap n= ")
+        n = inputIntNumber()
+        val array = IntArray(n)
+        i = 0
+        while (i < n) {
+            print("Nhap phan tu thu " + (i + 1) + " ")
+            array[i] = inputIntNumber()
+            i++
+        }
+        print("Cac phan tu trong day xuat hien 1 lan: ")
+        i = 0
+        while (i < n) {
+            if (countElement(array, n, array[i]) == 2) {
+                print("  " + array[i])
+            }
+            i++
+        }
+    }
+}
