7794a4fa0debdc742dd368d7406c321745a95c60
Vinh <vinh.nguyen@asiantech.vn>
2019-10-16 09:53:55 +0700

    List Fibonacci Numbers

---
+++ app/src/at-vinhnguyen/java/asiantech/internship/summer/exercise21/NumberFactorAnalysisAndCaculateSumOfNumber.kt
@@ -0,0 +1,17 @@
+package exercise21
+
+import doFactorAnalysis
+import inputIntNumber
+import sumOfNumber
+
+object NumberFactorAnalysisAndCaculateSumOfNumber {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        print("Nhap n")
+        val n = inputIntNumber()
+        print("n = 1.")
+        n.doFactorAnalysis()
+        println("Tong cac chu so cua " + n + " la: " + n.sumOfNumber())
+    }
+}
