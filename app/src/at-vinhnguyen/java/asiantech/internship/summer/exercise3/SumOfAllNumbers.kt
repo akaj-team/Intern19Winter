2e70cc023748baea491b9dfa5ec44b05d90ad476
Vinh <vinh.nguyen@asiantech.vn>
2019-10-16 09:47:03 +0700

    Sum of all number

---
+++ app/src/at-vinhnguyen/java/asiantech/internship/summer/exercise3/SumOfAllNumbers.kt
@@ -0,0 +1,14 @@
+package exercise3
+
+import inputIntNumber
+import sumOfNumber
+
+object SumOfAllNumbers {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        println("Nhap so: ")
+        val a = inputIntNumber()
+        println("Tong cac chu so: " + a.sumOfNumber())
+    }
+}
