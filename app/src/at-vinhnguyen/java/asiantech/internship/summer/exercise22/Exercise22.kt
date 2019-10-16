8c9e6546fdd2ab59642dc48204ae73d5d5370b34
Vinh <vinh.nguyen@asiantech.vn>
2019-10-16 09:54:15 +0700

    Exercise22

---
+++ app/src/at-vinhnguyen/java/asiantech/internship/summer/exercise22/Exercise22.kt
@@ -0,0 +1,40 @@
+package exercise22
+
+import inputIntNumber
+import isPrime
+
+object Exercise22 {
+    private fun doListDivisor(n: Int) {
+        var count = 0
+        print("\nCac uoc cua $n la:")
+        for (i in 1..n) {
+            if (n % i == 0) {
+                print(" $i")
+                count++
+            }
+        }
+        println("\nCo $count uoc")
+    }
+
+    private fun doListDivisorOfPrimeNumber(n: Int) {
+        var count = 0
+        print("\nCac uoc cua $n la:")
+        for (i in 1..n) {
+            if (n % i == 0 && i.isPrime()) {
+                print(" $i")
+                count++
+            }
+        }
+        println("\nCo $count uoc la so nguyen to")
+
+    }
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        print("Nhap n")
+        val n = inputIntNumber()
+        doListDivisor(n)
+        doListDivisorOfPrimeNumber(n)
+    }
+
+}
