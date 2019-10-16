ae75520790aea22830b9436ccb3f9fae745c88b4
Vinh <vinh.nguyen@asiantech.vn>
2019-10-16 09:54:34 +0700

    Exercise23

---
+++ app/src/at-vinhnguyen/java/asiantech/internship/summer/exercise23/Exercise23.kt
@@ -0,0 +1,39 @@
+package exercise23
+
+import inputIntNumber
+import isPrime
+
+object Exercise23 {
+
+    private fun listPrimeNumbers(n: Int) {
+        var i = 1
+        var count = 0
+        println("Cac so nguyen to nho hon $n la: ")
+        while (i < n) {
+            if (i.isPrime()) {
+                print(" $i")
+                count++
+            }
+            i++
+        }
+        println("\n Co $count so thoa man")
+    }
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        print("Nhap n")
+        val n = inputIntNumber()
+        listPrimeNumbers(n)
+        val f = IntArray(n)
+        f[0] = 1
+        f[1] = 1
+        var i = 1
+        print("Cac so Fibonanci nho hon $n la : \n 1")
+        while (f[i] < n) {
+            print(" " + f[i])
+            i++
+            f[i] = f[i - 1] + f[i - 2]
+        }
+        println("\n Co $i so thoa man")
+    }
+}
