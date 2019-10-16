25ffbf5ba0e5ae1b35d01fcf3e1b95dc453df061
Vinh <vinh.nguyen@asiantech.vn>
2019-10-16 09:47:57 +0700

    Prime Numbers Are Smaller Than N

---
+++ app/src/at-vinhnguyen/java/asiantech/internship/summer/exercise5/PrimeNumbersAreSmallerThanN.kt
@@ -0,0 +1,24 @@
+package exercise5
+
+import inputIntNumber
+import isPrime
+
+object PrimeNumbersAreSmallerThanN {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        println("Nhap n:")
+        val n = inputIntNumber()
+        println("Cac so nguyen to nho hon n bao gom: " + findPrimeNumbersAreSmallerThanN(n))
+    }
+
+    private fun findPrimeNumbersAreSmallerThanN(n: Int): String {
+        val builder = StringBuilder()
+        for (i in 2 until n) {
+            if (i.isPrime()) {
+                builder.append(i).append(" ")
+            }
+        }
+        return builder.toString()
+    }
+}
