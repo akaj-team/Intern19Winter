f8860f4c952a1d0dfe22599af780fc8f23769e54
= <=>
2019-10-17 08:15:15 +0700

    Exercise 5 kotlin

---
+++ app/src/at-nhatnguyen/java/asiantech/internship/summer/kotlin/Exercise5.kt
@@ -0,0 +1,26 @@
+package asiantech.internship.summer.kotlin
+import java.util.*
+fun main() {
+    val shareds = Shared()
+    val scan = Scanner(System.`in`)
+    print("Nhập n = ")
+    val n = scan.nextInt()
+    if (n == 1) {
+        println("Không có số nguyên tố nhỏ hơn 1 ")
+    } else {
+        System.out.printf("Tất cả các số nguyên tố nhỏ hơn %d là: \n", n)
+
+        if (n >= 2) {
+            print(2.toString())
+        }
+        var i = 3
+        while (i < n) {
+            if (shareds.isPrimeNumber(i)) {
+                print(" $i")
+            }
+            i += 2
+        }
+    }
+}
+
+
