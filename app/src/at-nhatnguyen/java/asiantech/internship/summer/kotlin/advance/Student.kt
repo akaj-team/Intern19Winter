0f06e5c09c5ebc9957d6570e5862d55440e0ec41
= <=>
2019-10-17 08:28:08 +0700

    Exercise advance kotlin, file Student

---
+++ app/src/at-nhatnguyen/java/asiantech/internship/summer/kotlin/advance/Student.kt
@@ -0,0 +1,16 @@
+package asiantech.internship.summer.kotlin.advance
+
+data class Student(
+        val id: Int,
+        val name: String,
+        val birthday: String,
+        val hometown: String,
+        val student_id: String,
+        val class_name: String,
+        val type: String,
+        val major: String,
+        val graduate_rank: String,
+        val title: String,
+        val degree_no: String,
+        val document_id: String
+)
