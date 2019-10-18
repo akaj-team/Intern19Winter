4c477308f8db87739d7d389356c687a1f6dc9b02
at-tamle <tam.le2@asiantech.vn>
2019-10-18 12:48:05 +0700

    bai27

---
+++ app/src/at-tamle/java/asiantech/internship/summer/Kotlin/Surname.kt
@@ -0,0 +1,28 @@
+package asiantech.internship.summer.Kotlin
+import java.util.Scanner
+import java.util.StringTokenizer
+object Surname {
+    private fun total(s: String) {
+        val t = StringTokenizer(s)
+        val k = StringBuffer()
+        val s1 = arrayOfNulls<String>(10)
+        var i = 1
+        while (t.hasMoreTokens()) {
+            s1[i] = t.nextToken()
+            i++
+        }
+        k.append(s1[3]).append("  ")
+        k.append(s1[1]).append("  ")
+        k.append(s1[2]).append("  ")
+        println(k)
+    }
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        var s0 = String()
+        val `in` = Scanner(System.`in`)
+        println("nhap xau ho ten: ")
+        s0 = `in`.nextLine()
+        total(s0)
+    }
+}
