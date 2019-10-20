188ee69b3afd6eabb90ba41a501acb56636c5fdd
at-myhuynh <my.huynh@asiantech.vn>
2019-10-20 17:00:08 +0700

    Create UI Login

---
+++ app/src/at-myhuynh/java/asiantech/internship/summer/kotlin/LoginActivity.kt
@@ -0,0 +1,13 @@
+package asiantech.internship.summer.kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import asiantech.internship.summer.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
