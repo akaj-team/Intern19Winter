345658af3ce4e99a679deaeeac5f8995aad2d1d1
Vinh <vinh.nguyen@asiantech.vn>
2019-10-21 11:19:57 +0700

    Move all class to kotlincore package

---
+++ app/src/at-vinhnguyen/java/asiantech/internship/summer/kotlincore/exercise3/SumOfAllNumbers.kt
@@ -0,0 +1,14 @@
+package asiantech.internship.summer.exercise3
+
+import inputIntNumber
+import sumOfNumber
+
+object SumOfAllNumbers {
+
+  @JvmStatic
+  fun main(args: Array<String>) {
+    println("Nhap so: ")
+    val a = inputIntNumber()
+    println("Tong cac chu so: " + a.sumOfNumber())
+  }
+}
