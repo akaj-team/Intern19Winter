cb00d94146cfa54ed525118dab7268155c6f32d2
= <=>
2019-10-21 09:21:01 +0700

    suport

---
+++ app/src/at-nhatnguyen/java/asiantech/internship/summer/layouteditor/CreateAccountActivity.kt
@@ -0,0 +1,13 @@
+package asiantech.internship.summer.layouteditor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import asiantech.internship.summer.R
+
+class CreateAccountActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_account)
+    }
+}
---
+++ app/src/at-nhatnguyen/java/asiantech/internship/summer/layouteditor/LoginActivity.kt
@@ -0,0 +1,16 @@
+package asiantech.internship.summer.layouteditor
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import asiantech.internship.summer.R
+import kotlinx.android.synthetic.`at-nhatnguyen`.activity_create_account.*
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+    }
+}
