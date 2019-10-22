be83cc4f3c5140c97e08ce058aa1e6161b59e203
at-tamle <tam.le2@asiantech.vn>
2019-10-22 14:37:37 +0700

    edtSign

---
+++ app/src/at-tamle/java/asiantech/internship/summer/New/SignActivity.kt
@@ -0,0 +1,13 @@
+package asiantech.internship.summer.New
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import asiantech.internship.summer.R
+
+class SignActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign)
+    }
+}
