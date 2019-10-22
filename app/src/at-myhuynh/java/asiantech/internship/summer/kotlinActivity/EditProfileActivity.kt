9924b02be79550a19c8acb9c1ced244b563b3177
at-myhuynh <my.huynh@asiantech.vn>
2019-10-22 09:47:10 +0700

    edit convention

--- app/src/at-myhuynh/java/asiantech/internship/summer/kotlinActivity/EditProfileActivity.kt
+++ app/src/at-myhuynh/java/asiantech/internship/summer/kotlinActivity/EditProfileActivity.kt
@@ -1,13 +1,13 @@
-package asiantech.internship.summer.kotlin
+package asiantech.internship.summer.kotlinActivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import asiantech.internship.summer.R
 
-class LoginActivity : AppCompatActivity() {
+class EditProfileActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_edit_profile)
     }
 }
--- app/src/at-myhuynh/java/asiantech/internship/summer/kotlinActivity/SignUpActivity.kt
+++ app/src/at-myhuynh/java/asiantech/internship/summer/kotlinActivity/SignUpActivity.kt
@@ -1,4 +1,4 @@
-package asiantech.internship.summer.kotlin
+package asiantech.internship.summer.kotlinActivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
