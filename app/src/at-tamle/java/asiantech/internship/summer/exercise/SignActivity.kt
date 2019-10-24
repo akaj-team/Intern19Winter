9efdd234881f15751fd50ead3e5ffd9f0c04f0b8
at-tamle <tam.le2@asiantech.vn>
2019-10-24 16:49:15 +0700

    camera intent

--- app/src/at-tamle/java/asiantech/internship/summer/exercise/SignActivity.kt
+++ app/src/at-tamle/java/asiantech/internship/summer/exercise/SignActivity.kt
@@ -1,13 +1,13 @@
-package asiantech.internship.summer.New
+package asiantech.internship.summer.exercise
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import asiantech.internship.summer.R
 
-class ProfileActivity : AppCompatActivity() {
+class SignActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
+        setContentView(R.layout.activity_sign)
     }
 }
