41379ad81ec12db8637cfa69ab5f1526acab05ef
at-tamle <tam.le2@asiantech.vn>
2019-10-31 12:38:38 +0700

    camera

--- app/src/at-tamle/java/asiantech/internship/summer/exercise/ProfileActivity.kt
+++ app/src/at-tamle/java/asiantech/internship/summer/exercise/ProfileActivity.kt
@@ -1,13 +1,42 @@
 package asiantech.internship.summer.exercise
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import asiantech.internship.summer.R
+import kotlinx.android.synthetic.`at-tamle`.activity_profile.*
 
 class ProfileActivity : AppCompatActivity() {
-
+    val CAMERA_REQUEST_CODE = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+        tvPicture.setOnClickListener()
+        {
+            val callCameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            if (callCameraIntent.resolveActivity(packageManager) != null) {
+                startActivityForResult(callCameraIntent, CAMERA_REQUEST_CODE)
+            }
+
+        }
     }
+
+    override fun onActivityResult(requestCode: Int, resultcode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultcode, data)
+        when (requestCode) {
+            CAMERA_REQUEST_CODE -> {
+                if (resultcode == Activity.RESULT_OK && data != null) {
+                    imgProfile.setImageBitmap(data.extras?.get("data") as Bitmap)
+                }
+            }
+            else -> {
+                Toast.makeText(this, "Unrecognized request code", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
 }
