345a1241c9a9684e1e27df510c059fe2cba743df
= <=>
2019-11-08 15:46:14 +0700

    Create TimeLineItem.kt

---
+++ app/src/at-nhatnguyen/java/asiantech/internship/summer/recyclerview/TimeLineItem.kt
@@ -0,0 +1,6 @@
+package asiantech.internship.summer.recyclerview
+
+class TimeLineItem(internal val photo: Int,
+                   internal var isLike: Boolean,
+                   internal var countLike:Int,
+                   internal var nickName:String)
