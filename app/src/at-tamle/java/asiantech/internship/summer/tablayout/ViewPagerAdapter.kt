bb61b5dcfd0b2be40bdee0c9cb40145ad745a3bd
at-tamle <Tamle135!>
2019-11-17 20:08:33 +0700

    tablayoutviewpaper

---
+++ app/src/at-tamle/java/asiantech/internship/summer/tablayout/ViewPagerAdapter.kt
@@ -0,0 +1,22 @@
+package asiantech.internship.summer.tablayout
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+
+
+class ViewPagerAdapter(val fm: FragmentManager, val fragments: List<Fragment>, val titles: List<String> = listOf())
+    : FragmentStatePagerAdapter(fm) {
+
+    override fun getItem(position: Int): Fragment {
+        return fragments.get(position)
+    }
+
+    override fun getCount(): Int {
+        return fragments.size
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return titles.get(position)
+    }
+}
